61324de397e17617cfa1b1be766356fad639135b
naujaras <gestionchatbotnaujaras@gmail.com>
2026-05-24 13:25:43 +0200

    Add files via upload

--- src/components/booking/StepContractSigning.tsx
+++ src/components/booking/StepContractSigning.tsx
@@ -35,7 +35,7 @@ export function StepContractSigning({ booking, onNext }: StepContractSigningProp
         {contractState === "opened" ? (
           <div className="w-full flex flex-col items-center">
             {contractUrl ? (
-              <div className="w-full h-[85vh] min-h-[900px] rounded-lg overflow-hidden border border-border shadow-inner bg-white">
+              <div className="w-full min-h-[1300px] h-[120vh] rounded-lg overflow-hidden border border-border shadow-inner bg-white">
                 <DocusealForm 
                   src={contractUrl}
                   
